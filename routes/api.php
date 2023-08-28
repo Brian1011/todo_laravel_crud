1d9826ee7005394cf793f1b9d15247e5679f67d3
Brian Mutinda <mutindabrian26@gmail.com>
2023-08-28 16:02:08 +0300

    make endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TaskCategoryController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// create endpoints for task categories, todos, and users
+Route::apiResource('task-categories', TaskCategoryController::class)->middleware('auth:passport');
+Route::apiResource('todos', TodoController::class)->middleware('auth:passport');
+
+// create an endpoint for registering a new user
+Route::post('register', [UserController::class, 'register']);
+// create an endpoint for logging in a user
+Route::post('login', [UserController::class, 'login']);
+// create an endpoint for logging out a user
+Route::post('logout', [UserController::class, 'logout'])->middleware('auth:passport');
 
